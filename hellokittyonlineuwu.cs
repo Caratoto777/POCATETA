6b39ea788f69e24c9bd5047d8de775499694aa3e
Caratoto777 <hamamar1603@gmail.com>
2024-11-29 17:02:59 +0100

    Add files via upload

---
+++ hellokittyonlineuwu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hellokittyonlineuwu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello kitty, online, only in cinema");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
